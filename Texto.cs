0acfb1b86e56f052380bc78408417bc8744fffdc
jaorcas <jaorcas_10@hotmail.com>
2018-09-22 12:40:50 +0200

    Add files via upload

---
+++ Texto.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class Texto : MonoBehaviour {
+
+
+    private void Start() {
+
+        Main();
+    }
+
+    static void Main() {
+
+
+        using (StreamReader read = new StreamReader(@"C:\prueba.txt")) {
+
+            string line = null;
+
+            while ((line=read.ReadLine() )!= null) {
+
+                File.AppendAllText(@"C:\prueba3.txt", line + "\r\n");
+
+            }         
+
+        }
+
+    }
+    
+}
