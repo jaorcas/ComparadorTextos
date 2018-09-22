f7aa5a4be8fcffdb5661fc3d77bf2bc1eb95bd9e
jaorcas <jaorcas_10@hotmail.com>
2018-09-22 13:40:06 +0200

    Update Texto.cs

--- Texto.cs
+++ Texto.cs
@@ -4,31 +4,40 @@ using UnityEngine;
 using System.IO;
 using System.Text;
 
-public class Texto : MonoBehaviour {
+public class Texto : MonoBehaviour{
 
 
-    private void Start() {
+    private void Start(){
 
         Main();
     }
 
-    static void Main() {
+    static void Main(){
 
 
-        using (StreamReader read = new StreamReader(@"C:\prueba.txt")) {
+        using (StreamReader leer = new StreamReader(@"C:\prueba.txt")){
 
-            string line = null;
+            string linea1 = null;
 
-            while ((line=read.ReadLine() )!= null) {
+            while ((linea1 = leer.ReadLine()) != null){                                  //LEE LA LINEA CORRESPONDIENTE DEL PRIMER TXT
 
-                File.AppendAllText(@"C:\prueba3.txt", line + "\r\n");
+                using (StreamReader leer2 = new StreamReader(@"C:\prueba2.txt")) {       //COGE ESA LÍNEA Y RECORRE EL SEGUNDO TXT
 
-                //hola
+                    string linea2 = null;
 
-            }         
+                    while ((linea2 = leer2.ReadLine()) != null){
+
+                        if (linea2 == linea1) {
+
+                            File.AppendAllText(@"C:\prueba3.txt", linea1 + "\r\n");     //SI LA LINEA DEL PRIMER TXT Y ALGUNA DEL SEGUNDO TXT COINCIDEN, LA ESCRIBE
+                        }
+
+                    }
+
+                }
+
+            }
 
         }
-
     }
-    
 }
